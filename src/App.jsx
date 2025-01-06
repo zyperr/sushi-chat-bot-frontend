b8f984260f8872845dad92f1caf44802b123b77d
Zyperr <maxi200vera@gmail.com>
2025-01-05 21:33:48 -0300

    se agregaron las rutas para las distintas paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Header from "./components/header/Header.jsx";
+import { ChatBot } from "./pages/ChatBot.jsx";
+import LoginPage from "./pages/Login.jsx"
+import MenuPage from "./pages/Menu.jsx";
+import RegisterPage from "./pages/Register.jsx"
 function App() {
 
   return (
-      <div>
-          hola
-      </div>
+      <BrowserRouter>
+          <Header/>
+          <Routes>
+              <Route path="/" element={<ChatBot />} />
+              <Route path="/login" element={<LoginPage/>} />
+              <Route path="/register" element={<RegisterPage/>} />
+              <Route path="/bot" element={<ChatBot />} />
+              <Route path="/menu" element={<MenuPage/>} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
