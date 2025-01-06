a68fadaa562beb60432d8a19ffd10945eaff5500
Zyperr <maxi200vera@gmail.com>
2025-01-05 21:36:36 -0300

    se creo pagina menu

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+import CardProduct from "../components/menu/CardProduct"
+import "../style/pages/Menu.css"
+const Menu = () => {
+  return (
+    <section className="section__menu">
+      <CardProduct />
+    </section>
+  )
+}
+
+export default Menu
