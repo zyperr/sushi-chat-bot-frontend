2eeba47e5f4174ca618b27e9445fee8943f55923
Zyperr <maxi200vera@gmail.com>
2025-01-05 21:37:16 -0300

    se crearon las rutas para iniciar sesion,registrar,menu y chatbot

---
+++ src/routes/route.js
@@ -0,0 +1,21 @@
+export const routesHeader = [
+    {
+        path: '/bot',
+        pathname: 'Chat Bot',
+    },
+    {
+        path: '/menu',
+        pathname: 'Menu',
+    },
+]
+
+export const userRoutes = [
+    {
+        path: '/register',
+        pathname:'Registrarse',
+    },
+    {
+        path: '/login',
+        pathname:'Iniciar sesion',
+    }
+]
